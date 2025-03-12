8c0e8d78a64fc7614930a8a2d3bcf3ba7c83b0e7
Guerrero-Valle-Marco <159593709+Guerrero-Valle-Marco@users.noreply.github.com>
2025-03-11 23:45:33 -0700

    Video Parte 6
    Avance hasta donde acaba el vídeo pero hay un error que no se resolver.

--- UI.gd
+++ UI.gd
@@ -1,16 +1,23 @@
 extends CanvasLayer
 
+const MIN_HEALTH: int = 23
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var max_hp: int = 4
+
+onready var player: KinematicBody2D = get_parent().get_node("Player")
+onready var health_bar: TextureProgress = get_node("HealthBar")
+onready var health_bar_tween: Tween = get_node("HealthBar/Tween")
+
+func _ready() -> void:
+	max_hp = player.hp
+	_update_health_bar(100)
+	
+	
+func _update_health_bar(new_value: int) -> void:
+	var __ = health_bar_tween.interpolate_property(health_bar, "value", health_bar.value, new_value, 0.5, Tween.TRANS_QUINT, Tween.EASE_OUT)
+	__ = health_bar_tween.start()
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Player_hp_changed(new_hp: int) -> void:
+	var new_health: int = int((100 - MIN_HEALTH) * float(new_hp) / max_hp) + MIN_HEALTH
+	_update_health_bar(new_health)
