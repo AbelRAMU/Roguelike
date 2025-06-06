0211220333070ed9fcb8bd60c3532327ef92a862
Guerrero-Valle-Marco <159593709+Guerrero-Valle-Marco@users.noreply.github.com>
2025-06-05 18:30:53 -0700

    Video Parte 11
    Ya acabamos el primer issue

--- Weapons/Weapon.gd
+++ Weapons/Weapon.gd
@@ -7,6 +7,7 @@ onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 onready var hitbox: Area2D = get_node("Node2D/Sprite/Hitbox")
 onready var charge_particles: Particles2D = get_node("Node2D/Sprite/ChargeParticles")
 onready var player_detector: Area2D = get_node("PlayerDetector")
+onready var tween: Tween = get_node("Tween") 
 
 
 func _ready() -> void:
@@ -51,3 +52,19 @@ func _on_PlayerDetector_body_entered(body):
 		player_detector.set_collision_mask_bit(1, false)
 		body.pick_up_weapon(self)
 		position = Vector2.ZERO
+	else:
+		var __ = tween.stop_all()
+		assert(__)
+		player_detector.set_collision_mask_bit(1, true)
+
+
+func interpolate_pos(initial_pos: Vector2, final_pos: Vector2) -> void:
+	var __ = tween.interpolate_property(self, "position", initial_pos, final_pos, 0.8, Tween.TRANS_QUART, Tween.EASE_OUT)
+	assert(__)
+	__ = tween.start()
+	assert(__)
+	player_detector.set_collision_mask_bit(0, true)
+
+
+func _on_Tween_tween_completed_(object: Object, _key: NodePath) -> void:
+	player_detector.set_collision_mask_bit(1, true)
