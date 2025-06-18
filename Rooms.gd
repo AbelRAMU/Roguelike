f3efc4c9b3b85ad582f2d1a09dd7ef29fff4c04f
porchi <jorge.castaneda22@cetis107.edu.mx>
2025-06-17 19:49:00 -0700

    Cambios parte 15 (creo)
    Una poción para recuperar vida y mejoras en la generación del mundo

--- Rooms.gd
+++ Rooms.gd
@@ -2,7 +2,9 @@ extends Navigation2D
 
 const SPAWN_ROOMS: Array = [preload("res://Rooms/SpawnRoom0.tscn"), preload("res://Rooms/SpawnRoom1.tscn")]
 const INTERMEDIATE_ROOMS: Array = [preload("res://Rooms/Room0.tscn"), preload("res://Rooms/Room1.tscn"), preload("res://Rooms/Room2.tscn"), preload("res://Rooms/Room3.tscn")]
+const SPECIAL_ROOMS: Array = [preload("res://Rooms/SpecialRoom0.tscn"), preload("res://Rooms/SpecialRoom1.tscn")]
 const END_ROOMS: Array = [preload("res://Rooms/EndRoom0.tscn")]
+
 
 const TILE_SIZE: int = 16
 const FLOOR_TILE_INDEX: int = 14
@@ -21,6 +23,8 @@ func _ready() -> void:
 func _spawn_rooms() -> void:
 	var previous_room: Node2D
 	
+	var special_room_spawned: bool = false 
+	
 	for i in num_levels:
 		var room: Node2D
 		
@@ -31,8 +35,12 @@ func _spawn_rooms() -> void:
 			if i == num_levels - 1:
 				room = END_ROOMS[randi() % END_ROOMS.size()].instance()
 			else:
-				room = INTERMEDIATE_ROOMS[randi() % INTERMEDIATE_ROOMS.size()].instance()
-				
+				if (randi() % 3 == 0 and not special_room_spawned) or (i == num_levels - 2 and not special_room_spawned):
+					room = SPECIAL_ROOMS[randi() % SPECIAL_ROOMS.size()].instance()
+					special_room_spawned = true
+				else:
+					room = INTERMEDIATE_ROOMS[randi() % INTERMEDIATE_ROOMS.size()].instance()
+					
 			var previous_room_tilemap: TileMap = previous_room.get_node("TileMap")
 			var previous_room_door: StaticBody2D = previous_room.get_node("Doors/Door")
 			var exit_tile_pos: Vector2 = previous_room_tilemap.world_to_map(previous_room_door.position) + Vector2.UP * 2
